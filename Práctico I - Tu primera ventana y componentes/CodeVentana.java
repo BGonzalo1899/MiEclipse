f5141d326a6405c60605158ee3f6f79a00487d70
BGonzalo1899 <bgonzalo490@gmail.com>
2025-07-25 22:23:59 -0300

    Add files via upload

---
+++ Práctico I - Tu primera ventana y componentes/CodeVentana.java
@@ -0,0 +1,55 @@
+package VentanaC;
+
+import java.awt.Color;
+
+import javax.swing.*;
+
+public class CodeVentana extends JFrame{
+
+
+	public CodeVentana() {
+		
+		JPanel panel = new JPanel ();
+		panel.setBackground(Color.green);
+		panel.setLayout(null);
+		JButton boton = new JButton("Aceptar");
+		boton.setBounds(120, 95 , 100, 20);
+		JLabel text = new JLabel("Bienvenido a mi programa");
+		text.setBounds(10, 10, 150, 50);
+		JLabel text2 = new JLabel("Ingresa tu nombre:");
+		text2.setBounds(10,40, 150, 50);
+		JTextField ingresa = new JTextField ();
+		ingresa.setBounds(120, 55 , 100, 20);
+		this.setTitle("Ventana de prueba - Botafogo 5 Penarol 0"); 
+		this.setSize(600, 400); 
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		this.setLocationRelativeTo(null); 
+		
+		this.add(panel);
+		panel.add(text);
+		panel.add(text2);
+		panel.add(ingresa);
+		panel.add(boton);
+		
+		 
+		
+		
+		
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Práctico I - Tu primera ventana y componentes/Ventana.java
@@ -0,0 +1,18 @@
+package VentanaC;
+import javax.swing.*;
+public class Ventana {
+
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		CodeVentana ventana = new CodeVentana();
+		ventana.setVisible(true);
+	
+	}
+
+	
+	
+}
