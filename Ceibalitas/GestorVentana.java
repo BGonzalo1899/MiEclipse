84f5def7b58c53901fb038505a17a818368481d1
BGonzalo1899 <bgonzalo490@gmail.com>
2025-08-28 21:31:28 -0300

    Add files via upload

---
+++ Ceibalitas/GestorVentana.java
@@ -0,0 +1,13 @@
+package PrototipoVisual;
+
+import javax.swing.JFrame;
+
+public class GestorVentana extends JFrame{
+
+	public static void main(String[] args) {
+		
+	MiVentana ventana = new MiVentana() ;	
+		ventana.setVisible(true);
+	}
+
+}
